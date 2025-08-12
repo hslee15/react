52e7476b4278c3ba9b9b268f4a48fc9efca43cda
hslee15 <lhs01019@kbu.ac.kr>
2025-08-12 15:06:15 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Counter from './components/Counter'
+import TodoList from './components/TodoList'
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <Counter/>
+      <TodoList/>
     </div>
   )
 }
